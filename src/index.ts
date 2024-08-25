a03821400c2c091443fd4cc075d8ad403451245a
nabendu82 <nabendu.biswas@gmail.com>
2024-08-25 08:47:47 +0530

    Final commit

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import { v4 as uuidV4 } from "uuid";
+
+const list = document.querySelector<HTMLUListElement>("#list");
+const form = document.querySelector<HTMLFormElement>("#task-form");
+const input = document.querySelector<HTMLInputElement>("#task-title");
+
+interface Task {
+  id: string
+  title: string
+  completed: boolean
+  createdAt: Date
+}
+
+form?.addEventListener("submit", e => {
+  e.preventDefault()
+  if(!input || input?.value === "" || input?.value === null) return
+
+  const newTask: Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date()
+  }
+
+  addItemToList(newTask)
+  input.value = ""
+})
+
+const addItemToList = (task: Task): void => {
+  const item = document.createElement("li");
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input");
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked
+    if(checkbox.checked) label.style.textDecoration = "line-through"
+  })
+  checkbox.type = "checkbox";
+  checkbox.checked = task.completed
+  label.append(checkbox, task.title)
+  item.append(label)
+  list?.append(item)
+}
